000ca3a3d4beb1780fc820821526064db6ba6f63
gvenegas <gvenegas@sionpower.com>
2024-07-09 15:53:21 -0700

    gui separation

--- tkpanels.py
+++ tkpanels.py
@@ -1,8 +1,7 @@
 """
 File: tkpanels.py
 Author: Gabe Venegas
-Purpose: Contains tkinter panels to be used 
-         in a serial logging & plotter app.
+Purpose: tkinter for serial logging & plotter app
 """
 
 import tkinter as tk
@@ -454,3 +453,51 @@ class FileBrowser(tk.Frame):
             
     def get_path(self):
         return self.label.cget("text")
+    
+class VSBGUI(tk.Tk):
+    """GUI frontend for VSB logger & plotter"""
+    
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+        
+        self.title("VSB Logger")
+        self.geometry("1400x700")
+        self.resizable(False, False)
+
+        self.controls = ControlsFrame(self)
+        self.controls.place(relx=0, rely=0, relwidth=0.45, relheight=0.3)
+        
+        self.filebrowser = FileBrowser(self)
+        self.filebrowser.place(relx=0, rely=0.3, relwidth=0.45, relheight=0.05)
+                
+        self.serial_setup = SerialSetup(self)
+        self.serial_setup.place(relx=0, rely=0.95, relwidth=0.45, relheight=0.05)
+
+        self.terminal = CLIFrame(self)
+        self.terminal.place(relx=0.01, rely=0.36, relwidth=0.43, relheight=0.58)
+        
+        # TODO Graph
+        # TODO Graph scroll
+        # TODO Data clear
+    
+    def bind_control(self, widget_name, func):
+        """Bind function to a GUI control panel button."""
+        self.controls.bind_func(widget_name, func)
+
+    def update_control(self, widget_name, data):
+        """Update widget on control panel with new data."""
+        self.controls.set_data(widget_name, data)
+        
+    def terminal_insert(self, data):
+        """Insert data into terminal."""
+        self.terminal.insert(data)
+        
+    def update_graph(self, datapoint: tuple):
+        """Add new data (x, y) to graph.
+        Plots and shifts graph if necessary."""
+        pass  # TODO
+
+if __name__ == "__main__":
+    # Example demo
+    app = VSBGUI()
+    app.mainloop()
