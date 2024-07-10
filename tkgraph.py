0fe4619f72a2b0b5d64a4d89700f8536eae1b8e7
gvenegas <gvenegas@sionpower.com>
2024-07-10 13:15:55 -0700

    multiline graph

--- tkgraph.py
+++ tkgraph.py
@@ -53,6 +53,22 @@ class LiveGraphFrame(tk.Frame):
         sub['y'].append(y)
         sub['line'].set_data(sub['x'], sub['y'])
         
+    def get_xdata(self, line_name):
+        """Get the xdata for a specific line."""
+        return self.lines[line_name]['x']
+    
+    def get_all_xdata(self):
+        """Get combined list of all xdata from all lines."""
+        return [x for sublist in [d['x'] for d in self.lines.values()] for x in sublist]
+    
+    def get_ydata(self, line_name):
+        """Get the ydata for a specific line."""
+        return self.lines[line_name]['y']
+    
+    def get_all_lines(self):
+        """Get all line objects from the lines dict."""
+        return (d['line'] for d in self.lines.values())
+            
     def put_data(self, channel, time, value):
         """Standardized function to put data into queue."""
         self.data_queue.put((channel, time, value))
@@ -61,22 +77,25 @@ class LiveGraphFrame(tk.Frame):
         """Standardized function to get data from queue."""
         return self.data_queue.get()
     
+    def clear_all_data(self):
+        """Clear a line's data from the graph."""
+        for line_name in self.lines.keys():
+            del self.lines[line_name]['x'][:]
+            del self.lines[line_name]['y'][:]
+    
     def init(self):
         """Init function for FuncAnimation."""
         self.ax.set_ylim(0, self.Y_HEIGHT)
         self.ax.set_xlim(0, self.X_WIDTH)
-        del self.xdata[:]
-        del self.ydata[:]
-        self.line.set_data(self.xdata, self.ydata)
-        return self.line,
+        self.clear_all_data()
+        return self.get_all_lines()
 
     def update(self, val):
         if not self.auto_shift:
-            
-            if len(self.xdata) > 1:
-                mx = max(self.xdata)
-                mn = min(self.xdata)
-                pos = mn + (self.spos.val * (mx - mn))
+            x = self.get_all_xdata()
+            if len(x) > 1:
+                mn = min(x)
+                pos = mn + (self.spos.val * (max(x) - mn))
                 self.ax.set_xlim(pos-self.X_WIDTH//2,pos+self.X_WIDTH//2)
                 self.ax.figure.canvas.draw()
             
@@ -86,19 +105,16 @@ class LiveGraphFrame(tk.Frame):
         
         while not self.data_queue.empty():
             channel, time, value = self.get_data()
-            
-            self.xdata.append(time)
-            self.ydata.append(value)
+            self.add_datapoint(channel, time, value)
         
-        if len(self.xdata) != 0:
-            mx = max(self.xdata)
+        all_x = self.get_all_xdata()
+        if len(all_x) > 0:
+            mx = max(all_x)
             if self.auto_shift and mx >= xmax:
                 self.ax.set_xlim(mx-self.X_WIDTH, mx)
                 self.ax.figure.canvas.draw()
-                
-            self.line.set_data(self.xdata, self.ydata)
 
-        return self.line,
+        return self.get_all_lines()
     
     def setup_widgets(self):
         """Setup the widgets for the Tkinter frame."""
@@ -108,10 +124,6 @@ class LiveGraphFrame(tk.Frame):
             self.auto_shift_button.config(text="Enable Scroller" if self.auto_shift else "Enable Auto-Shift")
             if not self.auto_shift:
                 self.update(0)  # Update with current slider value
-
-        def clear_data():
-            del self.xdata[:]
-            del self.ydata[:]
             
         self.config(width=500, height=500)  # basic size
         
@@ -122,7 +134,7 @@ class LiveGraphFrame(tk.Frame):
         self.auto_shift_button = tk.Button(self, text="Enable Scroller", command=toggle_auto_shift)
         self.auto_shift_button.place(relx=0, rely=0.95, relwidth=0.2, relheight=0.05)
             
-        self.clear_graph_button = tk.Button(self, text="Clear Graph", command=clear_data)
+        self.clear_graph_button = tk.Button(self, text="Clear Graph", command=self.clear_all_data)
         self.clear_graph_button.place(relx=0.2, rely=0.95, relwidth=0.2, relheight=0.05)
 
         self.toolbar = NavigationToolbar2Tk(self.canvas, self)
@@ -136,18 +148,21 @@ if __name__ == "__main__":
     import threading
     import time
     import random
+    import numpy as np
     
     def dummy_data_thread(graph: LiveGraphFrame):
         """Simulate graphing data coming in from a queue."""
         while True:
             for i in range(12):
-                graph.put_data(i, time.time_ns()/1e9, random.randint(0, 4095))
+                t = time.time_ns()/1e9
+                v = (np.sin(t) + i) * 4095/12
+                graph.put_data(i, t, v)
                 time.sleep(0.07)
     
     root = tk.Tk()
     
-    lgf = LiveGraphFrame(root)
-    lgf.pack()
+    lgf = LiveGraphFrame(root, refresh_ms=50)
+    lgf.pack(fill=tk.BOTH, expand=True)
     
     thread = threading.Thread(target=dummy_data_thread, args=(lgf,))
     thread.daemon = True
